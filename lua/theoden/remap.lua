18da253655080b6c394aba3b6bbdcc71636af85c
Wojciech Ziebicki <wojciech.ziebicki@gmail.com>
2023-04-10 08:24:08 +0200

    fix path to nvim config directory in remap.lua

--- lua/theoden/remap.lua
+++ lua/theoden/remap.lua
@@ -33,7 +33,7 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e " ..  vim.fn.expand("$HOME/nvim/lua/theoden/packer.lua") .. "<CR>");
+vim.keymap.set("n", "<leader>vpp", "<cmd>e " ..  vim.fn.expand("$HOME/.config/nvim/lua/theoden/packer.lua") .. "<CR>");
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
